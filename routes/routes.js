69155f9178a58fe6aff6aec7c26441758eb3e1c7
Nikolay Chistyakov <nikolay@chistyakov.me>
2017-02-26 19:32:35 +0300

    db work

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,38 @@
-module.exports = (function() {
+module.exports = function(app, database) {
     'use strict';
-    var router = require('express').Router();
     require('../extensions/dateformat');
-
+    //var router = app.Router;
 
     //Middle ware that is specific to this router
-    router.use(function timeLog(req, res, next) {
 
+    app.get('/', function(req, res) {
+        res.send('hello world');
+    });
+
+    app.use(function timeLog(req, res, next) {
         var date = new Date();
         console.log('Time: ', date.MyNewFunctionForDate());
         // console.log('Time: ', Date.now().toString());
         next();
-
-
     });
 
-    router.get('/', function(req, res) {
-        res.json({ 'foo': 'bar' });
+    app.post('/notes', (req, res) => {
+
+        // console.log(req.body)
+
+        const note = { text: req.body.body, title: req.body.title };
+        // database.collection('notes').insert()
+        var collectionNotes = database.collection('notes');
+
+        collectionNotes.insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+
+        // res.send('Hello')
     });
 
-    // // Define the home page route
-    // router.get('/', function(req, res) {
-    //     res.send('home page');
-    // });
-
-    router.post('/notes', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
-    });
-
-
-
-    return router;
-})();
+};
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
 const app = express();
-const routes = require('./routes/routes');
+
+const mongodb = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+// const routes = require('./routes/routes');
+const db = require('./config/db');
+
 
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', routes);
+// app.use('/', routes);
 
+mongodb.connect(db.url, (err, database) => {
 
-app.listen(port, () => {
-    console.log('We are live on ' + port);
-});
+    if (err) {
+        return console.log(err);
+    }
+    require('./routes/routes')(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+
+})
+
+// routes(app);
+
+//require('./routes/routes')("2352134632456");
