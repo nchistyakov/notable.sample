9c98a915985bc6863ba555fdc648216a3da4729b
Nikolay Chistyakov <nikolay@chistyakov.me>
2017-02-26 19:40:04 +0300

    db findOne

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,20 @@
 module.exports = function(app, database) {
     'use strict';
     require('../extensions/dateformat');
-    //var router = app.Router;
-
-    //Middle ware that is specific to this router
+    var ObjectID = require('mongodb').ObjectID;
 
     app.get('/', function(req, res) {
         res.send('hello world');
     });
 
-    app.use(function timeLog(req, res, next) {
+    app.use('/timeLog', function timeLog(req, res, next) {
         var date = new Date();
         console.log('Time: ', date.MyNewFunctionForDate());
-        // console.log('Time: ', Date.now().toString());
         next();
     });
 
     app.post('/notes', (req, res) => {
-
-        // console.log(req.body)
-
         const note = { text: req.body.body, title: req.body.title };
-        // database.collection('notes').insert()
         var collectionNotes = database.collection('notes');
 
         collectionNotes.insert(note, (err, result) => {
@@ -32,7 +25,18 @@ module.exports = function(app, database) {
             }
         });
 
-        // res.send('Hello')
+    });
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        database.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
     });
 
 };
